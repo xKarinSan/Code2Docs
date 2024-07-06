1f449c0fb7b3b0bda9f85dc62d4bfd2968a15a15
Siang Meng <siang-meng@live.com>
2024-07-06 16:24:41 +0800

    [Backend] Chains in langchain

--- backend/src/services/file_reader/routes.py
+++ backend/src/services/file_reader/routes.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, UploadFile
 from services.file_reader.file_reader import file_reader
-from services.document_serivice.document_service import document_service
+from services.document_service.document_service import document_service
 
 router = APIRouter()
 
 
 @router.post("/")
-def upload_zip_file(file: UploadFile):
+def summarise_zip_file(file: UploadFile):
     try:
         document_service.unzip_file(file.file)
-        document_service.summarise_files()
-        return {"message": "uploaded"}
+        res = document_service.summarise_files()
+        return {"data": res}
+
     except Exception as e:
-        print("e",e)
+        return {"error": e}
