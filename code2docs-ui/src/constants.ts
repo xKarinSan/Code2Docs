746901627453ca0575ffd494bdff3607829f5d8a
Siang Meng <siang-meng@live.com>
2024-10-10 21:53:08 +0800

    [Frontend][Feat] API integration for Github Repo Access

--- code2docs-ui/src/constants.ts
+++ code2docs-ui/src/constants.ts
@@ -1,8 +1,19 @@
 const githubAuthClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
 const githubAuthURL = `https://github.com/login/oauth/authorize?client_id=${githubAuthClientId}`;
-const githubAppInstallURL = `https://github.com/apps/${import.meta.env.VITE_GITHUB_APP_APP_NAME}/installations/new`;
+const githubAppInstallURL = `https://github.com/apps/${
+    import.meta.env.VITE_GITHUB_APP_APP_NAME
+}/installations/new`;
+
+// backend URLs
 const backendURL = import.meta.env.VITE_API_URL;
 const getGithubTokenURL = backendURL + "/gh/login/?code=";
 const getGithubAppTokenURL = backendURL + "/gh/install/?installation_id=";
+const getUserGithubRepoURL = backendURL + "/gh/repos/u/";
 
-export { githubAuthURL, getGithubTokenURL, githubAppInstallURL ,getGithubAppTokenURL};
+export {
+    githubAuthURL,
+    getGithubTokenURL,
+    githubAppInstallURL,
+    getGithubAppTokenURL,
+    getUserGithubRepoURL,
+};
--- code2docs-ui/src/pages/codebases/CodeBaseListPage.tsx
+++ code2docs-ui/src/pages/codebases/CodeBaseListPage.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useUserStore } from "../../store/userStore";
 
+import { getUserGithubRepoURL } from "../../constants";
 function CodeBaseListPage() {
+    const githubUsername = useUserStore((state: any) => state.githubUsername);
+    const githubAppToken = useUserStore((state: any) => state.githubAppToken);
+
+    const [codebases, setCodebases] = useState([]);
+    const getUserRepos = async (pageNumber: number = 1) => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${githubAppToken}`,
+            },
+        };
+        console.log(`githubUsername ${githubUsername}`);
+        await axios
+            .get(
+                `${getUserGithubRepoURL}${githubUsername}?page_num=${pageNumber}`,
+                config
+            )
+            .then((res) => {
+                return res.data;
+            })
+            .then((data) => {
+                const { repos } = data;
+                console.log(`Repos`, repos);
+                setCodebases(repos);
+            });
+    };
+
+    useEffect(() => {
+        getUserRepos();
+    }, []);
     return <div>Codebases</div>;
 }
 
