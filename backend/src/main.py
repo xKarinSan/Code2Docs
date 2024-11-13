c32bcbebc5b1c02492de4674902b0a70d918acd2
Siang Meng <siang-meng@live.com>
2024-11-13 20:14:38 +0800

    [Backend][Pipeline] python imports

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../')
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from backend.src.services.document_service.app import document_service_app
