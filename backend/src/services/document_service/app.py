a3e4f418b49dcf0fbaaa158c6b78edab5d3fbe71
Siang Meng <siang-meng@live.com>
2024-11-20 05:15:52 +0800

    Revert "[Backend] Add cors for other subapps"
    This reverts commit 214bcfaa629f69f018383823344a7869b7d03f3d.

--- backend/src/services/document_service/app.py
+++ backend/src/services/document_service/app.py
@@ -1,14 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
 from backend.src.services.document_service.routes import router
 
 document_service_app = FastAPI()
 document_service_app.include_router(router)
-document_service_app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
