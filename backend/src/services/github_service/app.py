214bcfaa629f69f018383823344a7869b7d03f3d
Siang Meng <siang-meng@live.com>
2024-11-20 05:00:29 +0800

    [Backend] Add cors for other subapps

--- backend/src/services/github_service/app.py
+++ backend/src/services/github_service/app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.src.services.github_service.routes import router
 import time
 
 github_service_app = FastAPI()
 github_service_app.include_router(router)
+github_service_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
