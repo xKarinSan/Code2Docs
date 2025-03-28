b48243c7681135e273d91f93634299d28ed39923
Siang Meng <siang-meng@live.com>
2025-03-28 23:31:43 +0800

    allow saving and displaying of key in environment

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
 import typer
 
+
 app = typer.Typer()
+
+
+@app.command("upload-key")
+def upload_key(key: str):
+    """
+    To upload an API key
+    """
+    if not key:
+        print("Key not found")
+    else:
+        env_path = Path(".env")
+        with env_path.open("w") as f:
+            f.write(f"api_key={key}")
+        print("key saved")
+
+
+@app.command("show-key")
+def show_key(masked: bool = True):
+    """
+    To show if the key is present or not. NOTE: This should at least be masked during production.
+    """
+    curr_key = os.environ.get("api_key", None)
+    if not curr_key:
+        print("Key not found")
+    else:
+        if masked:
+            masked = curr_key[:2] + "*" * (len(curr_key) - 4) + curr_key[-2:]
+            print(f"Masked key is: {masked}")
+        else:
+            print(f"Unmasked key is:{curr_key}")
 
 
 @app.command("wave")
@@ -24,4 +58,5 @@ def goodbye():
 
 
 if __name__ == "__main__":
+    load_dotenv()
     app()
