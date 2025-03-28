00ac41c6fe680443045955497ca501c2d03e3a4d
Siang Meng <siang-meng@live.com>
2025-03-28 23:12:08 +0800

    changed names and descriptions of commands

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import typer
 app = typer.Typer()
 
 
-@app.command()
-def hello(name: str, iq: int, display_iq:bool = True):
+@app.command("wave")
+def hello(name: str, iq: int, display_iq: bool = True):
+    """
+    To say hello
+    name (string): name of person
+    iq (int): iq of person
+    """
     print(f"Hello {name}")
     if display_iq:
         print(f"This is your IQ: {iq}")
 
 
-@app.command()
+@app.command("bye")
 def goodbye():
+    """
+    To say goodbye
+    """
     print("Goodbye")
 
 
