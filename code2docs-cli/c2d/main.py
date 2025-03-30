686395d45999d8b1764a7e9b794df225db05e662
Siang Meng <siang-meng@live.com>
2025-03-30 23:46:51 +0800

    changed the cli name

---
+++ code2docs-cli/c2d/main.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+import sys
+import os
+from dotenv import load_dotenv
+import typer
+from rich import print
+from rich.console import Console
+from rich.panel import Panel
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command("upload-key")
+def upload_key(key: str):
+    """
+    \b
+    To upload an API key.
+    \nParameters:
+      - key (string): the value of the API key
+    \nExample:
+      c2d upload-key test_key
+    """
+    if not key:
+        print("Key not found")
+    else:
+        env_path = Path(".env")
+        env_path.parent.mkdir(parents=True, exist_ok=True)
+        with env_path.open("w") as f:
+            f.write(f"api_key={key}")
+        print("key saved")
+
+
+@app.command("show-key")
+def show_key(masked: bool = True):
+    """
+    To show if the key is present or not. NOTE: This should at least be masked during production.
+    """
+    curr_key = os.environ.get("api_key", None)
+    if not curr_key:
+        print("Key not found")
+    else:
+        if masked:
+            masked = curr_key[:2] + "*" * (len(curr_key) - 4) + curr_key[-2:]
+            print(f"Masked key is: {masked}")
+        else:
+            print(f"Unmasked key is:{curr_key}")
+
+
+@app.command("scan")
+def scan_directory():
+    """
+    \b
+    This scans the current directory which the C2D cli is at
+
+    """
+    print("Scanning ...")
+    path = os.curdir
+    
+    print(os.listdir(path))
+    return
+
+
+@app.command("wave")
+def hello(name: str, iq: int, display_iq: bool = True):
+    """
+    \b
+    To say hello. here are the following parameters
+    \nParameters:
+      - name (string): Name of the person
+      - iq (int): IQ of the person
+      - display_iq (bool, optional): Whether to display the IQ (default: True)
+    \nExample:
+      c2d wave John 130 --display-iq
+    """
+    print(f"Hello {name}")
+    if display_iq:
+        print(f"This is your IQ: {iq}")
+
+
+def main():
+    if len(sys.argv) == 1:
+        console.print(
+            Panel.fit(
+                "[bold cyan]Welcome to Code2Docs CLI 🚀[/bold cyan]\n\n"
+                "Use [green]--help[/green] after any command to see usage.",
+                title="📘 Code2Docs",
+                border_style="bright_blue",
+            )
+        )
+        return
+    load_dotenv()
+    app()
+
+
+if __name__ == "__main__":
+    main()
